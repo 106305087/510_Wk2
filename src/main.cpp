1391ba75e3c8fe5fdbdbac0c6c12f04715834225
Chou <choupei-hsuan@ChoudeMacBook-Air.local>
2024-01-12 17:18:59 -0800

    this is the initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#define ANALOG_PIN D1
+
+// put function declarations here:
+// int myFunction(int, int);
+
+void setup() {
+  Serial.begin(115200); // Start the serial communication
+  pinMode(ANALOG_PIN, INPUT);
+}
+
+void loop() {
+  int analogValue = analogRead(ANALOG_PIN); // Read the analog value
+  float Voltage = 3.3 * (analogValue / 4095.0); 
+  Serial.print("ADC_Value: ");
+  Serial.print(analogValue);
+  Serial.print(" Voltage: ");
+  Serial.println(Voltage); // 'println' adds a new line at the end// Print the value to the serial monitor
+  delay(1000); // Wait for a second
+}
+
+// put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
